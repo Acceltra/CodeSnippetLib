af65b443fc9f05e03148f3c2ab2e0dc6c93a7681
Acceltra <acceltra521@gmail.com>
2024-07-14 21:38:45 +0800

    add image erode

--- ImageProcessSnippetLib/PythonImpl/MorphologyFilter.py
+++ ImageProcessSnippetLib/PythonImpl/MorphologyFilter.py
@@ -2,10 +2,26 @@ import cv2
 import numpy as np
 import PythonCommonTools
 
-def build_kernel(width, height):
-    kernel = np.ones(width, height)
-    return kernel
 
-            
+def image_erode(image, kernel):
+    kernel_w = kernel.shape[0]
+    kernel_h = kernel.shape[1]
+    kernel_wing_w = int((kernel_w - 1) / 2)
+    kernel_wing_h = int((kernel_h - 1) / 2)
+    start_pos_x = kernel_wing_w
+    start_pos_y = kernel_wing_h
+    res_image = np.zeros(image.shape)
+    image = cv2.copyMakeBorder(image, kernel_wing_h, kernel_wing_h, kernel_wing_w, kernel_wing_w, cv2.BORDER_REPLICATE)
+
+    for i in range(start_pos_x, image.shape[0] - start_pos_x - 1):
+        for j in range(start_pos_y, image.shape[1] - start_pos_y - 1):
+            sum_val = image[i - kernel_wing_w: i + kernel_wing_w + 1, j - kernel_wing_h: j + kernel_wing_h + 1]
+            print(np.sum(sum_val * kernel))
+            if np.sum(sum_val * kernel) < 255 * 9:
+                res_image[i, j] = 0
+            else:
+                res_image[i, j] = image[i, j]
+
+    return res_image
 
 
--- ImageProcessSnippetLib/PythonImpl/PythonCommonTools.py
+++ ImageProcessSnippetLib/PythonImpl/PythonCommonTools.py
@@ -1,22 +1,27 @@
 import cv2
+import numpy as np
+
 
 def show_image(image):
     cv2.imshow('show_image', image)
     cv2.waitKey(0)
 
+
 def rgb_to_gray_cv(image):
-    image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     return image
+
 
 def rgb_to_binary_cv(image):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     ret, image = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
     return image
 
-def make_border(image, top, bottom, left, right):
-    image = cv2.copyMakeBorder(image, top, bottom, left, right, cv2.BORDER_REPLICATE)
+
+def make_kernel(width, height):
+    kernel = np.ones((width, height), int)
+    return kernel
+
+
+def make_border_alg(image):
     return image
-
-
-
-
